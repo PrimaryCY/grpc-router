3cfe51db4397866f82f1722a515390e68f17cea9
CY <907031027@qq.com>
2021-09-09 12:14:12 +0800

    add: 完成coordinate抽象类

--- coordinate/base.go
+++ coordinate/base.go
@@ -1,1 +1,13 @@
 package coordinate
+
+type (
+	BaseCoordinateManager interface {
+		RegisterHttp(...interface{}) string
+		RegisterWs(...interface{}) string
+		RegisterRpc(...interface{}) string
+		DeregisterHttp(string)
+		DeregisterWs(string)
+		DeregisterRpc(string)
+		GetHttpService()interface{}
+	}
+)
